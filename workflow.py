6932c6c5befb876fd277ba50cdbd5871ce16a4c2
Manuel Cabeza Gallucci <42854238+ManuelCabeza@users.noreply.github.com>
2022-11-18 17:58:47 +0100

    added import data function

---
+++ workflow.py
@@ -0,0 +1,14 @@
+
+
+def import_data(csv_path, col_names, normalize="std", dummy_cols=None, replace_nan="mean"):
+    """
+    csv_path: path to the data in csv format
+    col_names: names of the columns of the dataset
+    normalize: Normalization type for all columns. ("std", "0_1")\
+    dummy_cols: names of the columns to apply dummy variables
+    replace_nan: Way to replace nan or missing values. ("mean", "median", "remove")
+    """
+
+
+    return data
+
