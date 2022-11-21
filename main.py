cc3e47a12294f0efebe0e29fc9e80d11bd58d2df
Manuel Cabeza Gallucci <42854238+ManuelCabeza@users.noreply.github.com>
2022-11-21 18:29:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ def main():
     
     BankNotes = {
         "data_path": "./data/data_banknote_authentication.txt",
-        "ftr_names": ,
+        "ftr_names": ["variance of WTI", "skewness of WTI", "curtosis of WTI", "entropy"],
         "out_path": "./output/bank_notes",
         "model": RandomForestClassifier(n_estimators=10, max_depth=6),
         "cross_validation": 5,
         "test_size": 0.3,
         "true_label": ( , ),
         "dummy_cols": None,
-    }
+        } 
     
-    ChronicKidney = {
-        "data_path": "./data/kidney_disease.csv",
-        "ftr_names": [""],
-        "out_path": "./output/chronic_kidney",
-        "model": RandomForestClassifier(n_estimators=10, max_depth=6),
-        "cross_validation": 5,
-        "test_size": 0.3,
-        "true_label": ( , ),
-        "dummy_cols": ,
-    }
+    # ChronicKidney = dict(
+    #     "data_path": "./data/kidney_disease.csv",
+    #     "ftr_names": [""],
+    #     "out_path": "./output/chronic_kidney",
+    #     "model": RandomForestClassifier(n_estimators=10, max_depth=6),
+    #     "cross_validation": 5,
+    #     "test_size": 0.3,
+    #     "true_label": ( , ),
+    #     "dummy_cols": , )
 
     workflow = [BankNotes, ChronicKidney]
 
     for work in workflow:
 
         data, labels = wf.import_data(work["data_path"], work["ftr_names"], true_label_str=work["true_label"], dummy_cols=None)
-        
+
+        print(data)
 
         break
 if __name__ == "__main__":
