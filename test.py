50cda6c330b4c97bab02bc83c9e5904da166c21d
Manuel Cabeza Gallucci <42854238+ManuelCabeza@users.noreply.github.com>
2022-12-04 23:07:13 +0100

    Corrected random state in tests

--- test.py
+++ test.py
@@ -7,18 +7,20 @@ class TestSum(unittest.TestCase):
     def test_import_data_replacements(self):
         replace_values = [True, False]
         for replace_value in replace_values:
-            data, label = wf.import_data("./data/test/test_data.csv", ["dummy", "int", "float", "label"], true_label_str=None,normalize=None, class_label="last", dummy_cols=["dummy"], replace_nan=replace_value)
+            data, label = wf.import_data("./data/test/test_data.csv", ["dummy", "int", "float", "label"], true_label_str=None,normalize=None, class_label="last", dummy_cols=["dummy"], replace_nan=replace_value, random_state=None)
             
             correct_result = pd.read_csv("./data/test/test_result_"+str(replace_value)+".csv")
             
             if replace_value:
                 correct_label = np.array([1,1,1,1,1,0])
                 dataBool = data.reset_index(drop=True) == correct_result.reset_index(drop=True)
+                
                 self.assertEqual(dataBool.all().all(), True)
                 self.assertEqual(np.array_equal(label, correct_label), True)
             else:
                 correct_label = np.array([1,1,1,1,0])
                 dataBool = data.reset_index(drop=True) == correct_result.reset_index(drop=True)
+                
                 self.assertEqual(dataBool.all().all(), True)
                 self.assertEqual(np.array_equal(label, correct_label), True)
 
